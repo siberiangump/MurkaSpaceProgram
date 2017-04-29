cef3e10bb56bda08cb1e8f6266718a4cbc63282e
Sokolov Anton <sokolov.anton@murka.com>
2017-04-29 15:27:58 +0300

    [+] Data Parser

---
+++ Assets/Scripts/DataParsing/ParseToObjectData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParseToObjectData : MonoBehaviour
+{
+
+	[SerializeField] ObjectData Target;
+    [SerializeField] string FileName;
+
+    [ContextMenu ("Parse")]
+    public void Parse()
+    {
+    }
+
+}
