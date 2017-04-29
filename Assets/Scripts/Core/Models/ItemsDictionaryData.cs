1a34d0e2c8dfccd6afb5f1fc3f152b4311bf768b
Андрей Сеньчов <senchov@gmail.com>
2017-04-29 14:45:02 +0300

    to prev commit

---
+++ Assets/Scripts/Core/Models/ItemsDictionaryData.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu (fileName = "ItemsDictionaryData", menuName = "OfferConfig/ScriptableObject/ItemsDictionaryData", order = 1231527)]
+public class ItemsDictionaryData : ScriptableObject
+{
+    
+    [SerializeField] ItemData[] Items;
+
+    public GameObject GetGameObject(string id)
+    {
+        GameObject gmo = null;
+        for (int i = 0; i < Items.Length; i++)
+        {
+            if(Items[i].Id == id)
+                return Items[i].Item;
+        }
+        return gmo;
+    }
+
+    public GameObject GetGameObject(int id)
+    {
+        return GetGameObject(id.ToString());
+    }
+
+    [System.Serializable]
+    public class ItemData
+    {
+        public string Id;
+        public GameObject Item;
+    }
+}
+
---
+++ Assets/Scripts/Core/Models/ObjectData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectData
+{
+    public string Id;
+    public string PrefabId;
+    public TransferTrajectoryData[] TrajectoryDataSet;
+}
