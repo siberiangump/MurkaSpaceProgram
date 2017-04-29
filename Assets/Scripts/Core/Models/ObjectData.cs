3cdf55adbb5daba6b71932195cce23e70b737e0d
Valentin Bezshchasniy <valentin.bezshhasnyj@murka.com>
2017-04-29 16:09:36 +0300

    parse data

--- Assets/Scripts/Core/Models/ObjectData.cs
+++ Assets/Scripts/Core/Models/ObjectData.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,5 +7,5 @@ public class ObjectData : ScriptableObject
 {
     public string Id;
     public GameObject PrefabId;
-    public TransferTrajectoryData[] TrajectoryDataSet;
+    public List <TransferTrajectoryData> TrajectoryDataSet;
 }
--- Assets/Scripts/DataParsing/ParseToObjectData.cs
+++ Assets/Scripts/DataParsing/ParseToObjectData.cs
@@ -1,16 +1,42 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class ParseToObjectData : MonoBehaviour
 {
 
-	[SerializeField] ObjectData Target;
-    [SerializeField] string FileName;
+    [SerializeField]
+    ObjectData Target;
+    [SerializeField]
+    string FileName;
 
-    [ContextMenu ("Parse")]
+    [ContextMenu("Parse")]
     public void Parse()
     {
-    }
+        StreamReader reader = File.OpenText(FileName);
+        string line;
+        bool init = true;
+        while ((line = reader.ReadLine()) != null)
+        {
+            if (init)
+            {
+                init = false;
+                continue;
+            }
 
+            TransferTrajectoryData tmp = new TransferTrajectoryData();
+            string[] items = line.Split(new string[] { "   " }, StringSplitOptions.RemoveEmptyEntries);
+            DateTime date;
+            DateTime.TryParse(items[0], out date);
+            tmp.UTCTime = date.ToFileTimeUtc();
+            Double.TryParse(items[1], out tmp.X);
+            Double.TryParse(items[2], out tmp.Y);
+            Double.TryParse(items[3], out tmp.Z);
+
+            Target.TrajectoryDataSet.Add(tmp);
+        }
+
+    }
 }
