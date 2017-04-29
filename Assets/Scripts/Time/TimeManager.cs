d6a31e1a0e6a7102be6b51af15f64ff1b067d0ee
Yaroslav Khodakovskiy <jaroslav.khodakovskiy@murka.com>
2017-04-29 14:54:25 +0300

    Basic timeline functionality

---
+++ Assets/Scripts/Time/TimeManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField]
+    private GlobalTimeData TimeData;
+    [SerializeField]
+    private TimelineSegmentsController Segments;
+    [SerializeField]
+    private Slider Slider;
+
+    private Action<float> OnTick;
+
+    private float TimeValue
+    {
+        get
+        {
+            return Time.time;
+        }
+    }
+
+    void Start()
+    {
+        SetTimeData(TimeData);
+    }
+
+    void Update()
+    {
+        if (OnTick != null)
+            OnTick(Slider.value);
+    }
+
+    public void SubscribeOnTick(Action<float> func)
+    {
+        OnTick += func;
+    }
+
+    public void SetTimeData(GlobalTimeData data)
+    {
+        TimeData = data;
+        Segments.Initialize(TimeData);
+        Slider.minValue = data.StartTime;
+        Slider.maxValue = data.EndTime;
+    }
+}
+
+[System.Serializable]
+public struct GlobalTimeData
+{
+    public float StartTime;
+    public float EndTime;
+    public float Step;
+}
---
+++ Assets/Scripts/Time/TimelineSegment.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimelineSegment : MonoBehaviour
+{
+    [SerializeField]
+    private float SliderWidth;
+    [SerializeField]
+    private Text Text;
+
+    public void SetData(TimelineSegmentData data)
+    {
+        RectTransform rTransform = transform as RectTransform;
+        rTransform.localPosition = new Vector3((data.Position - .5f) * (SliderWidth - 22), 1);
+
+        Text.text = data.Text;
+    }
+}
+
+[System.Serializable]
+public struct TimelineSegmentData
+{
+    public float Position;
+    public string Text;
+}
